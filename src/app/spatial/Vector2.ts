3d908b511f6299e795adb7aeab0db27b2b89bcac
Tyler Coles <shidobu@gmail.com>
2019-03-16 14:50:15 -0400

    Implemented cloneable for vectors.

--- src/app/spatial/Vector2.ts
+++ src/app/spatial/Vector2.ts
@@ -1,6 +1,6 @@
+import { ICloneable } from "../../lib/lang";
 
-
-export class Vector2 {
+export class Vector2 implements ICloneable<Vector2> {
     private x: number = 0;
 
     private y: number = 0;
@@ -29,6 +29,15 @@ export class Vector2 {
     }
 
     public static get Zero(): Vector2 {
-        return Vector2.zero;
+        return Vector2.zero.clone();
+    }
+
+    public set(x: number, y: number): void {
+        this.x = x;
+        this.y = y;
+    }
+
+    clone(): Vector2 {
+        return new Vector2(this.x, this.y);
     }
 }
