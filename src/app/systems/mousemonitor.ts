04579655a9c800e7acfe850ac6469ab089665dd8
Tyler Coles <shidobu@gmail.com>
2019-03-16 12:54:41 -0400

    Added mousedown/mouseup event handlers.

--- src/app/systems/mousemonitor.ts
+++ src/app/systems/mousemonitor.ts
@@ -1,6 +1,26 @@
 import { System } from "../../lib/ents/system";
+import { CanvasContextHelper } from "../helpers/canvascontext";
 
 
 export class MouseMonitor extends System {
 
+    constructor() {
+        super();
+
+        let canvas = CanvasContextHelper.getCanvas();
+        canvas.addEventListener("mousedown", (e) => this.onMouseDown(e), false);
+        canvas.addEventListener("mouseup", (e) => this.onMouseUp(e), false);
+    }
+
+    onMouseDown(event: MouseEvent): void {
+        console.log(event);
+    }
+
+    onMouseUp(event: MouseEvent): void {
+        console.log(event);
+    }
+
+    before(): void {
+
+    }
 }
