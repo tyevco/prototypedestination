153a5a77da76b2e87e90e0200d30599b51dfef21
Tyler Coles <shidobu@gmail.com>
2019-03-16 11:51:41 -0400

    Merged location into physics.

--- src/app/components/location.ts
+++
@@ -1,6 +0,0 @@
-import { Component } from "../../lib/ents/component";
-import { Vector2 } from "../spatial/Vector2";
-
-export default class Location extends Component {
-    public Position: Vector2;
-}
--- src/app/components/physics.ts
+++ src/app/components/physics.ts
@@ -4,4 +4,6 @@ import { Vector2 } from "../spatial/Vector2";
 
 export default class Physics extends Component {
     public Velocity: Vector2;
+    public Rotation: number;
+    public WorldPosition: Vector2;
 }
