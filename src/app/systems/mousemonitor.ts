f9ed3b2f55ce51ad0fe129dcc13e71c16029ed44
Tyler Coles <shidobu@gmail.com>
2019-03-16 13:06:58 -0400

    Implemented MouseHandler in MouseMonitor.

--- src/app/systems/mousemonitor.ts
+++ src/app/systems/mousemonitor.ts
@@ -1,23 +1,43 @@
 import { System } from "../../lib/ents/system";
 import { CanvasContextHelper } from "../helpers/canvascontext";
+import { MouseHandler } from "../../lib/lang";
 
 
-export class MouseMonitor extends System {
+export class MouseMonitor extends System implements MouseHandler {
+    mouseDown: boolean = false;
 
     constructor() {
         super();
 
         let canvas = CanvasContextHelper.getCanvas();
-        canvas.addEventListener("mousedown", (e) => this.onMouseDown(e), false);
-        canvas.addEventListener("mouseup", (e) => this.onMouseUp(e), false);
+        canvas.addEventListener("mousedown", e => this.onMouseDown(e), false);
+        canvas.addEventListener("mouseup", e => this.onMouseUp(e), false);
+        canvas.addEventListener("mousemove", e => this.onMouseMove(e), false);
+        canvas.addEventListener("mouseenter", e => this.onMouseEnter(e), false);
+        canvas.addEventListener("mouseleave", e => this.onMouseLeave(e), false);
     }
 
     onMouseDown(event: MouseEvent): void {
         console.log(event);
+
+        this.mouseDown = true;
+
     }
 
     onMouseUp(event: MouseEvent): void {
         console.log(event);
+
+        this.mouseDown = false;
+    }
+
+    onMouseMove(event: MouseEvent): void {
+
+    }
+
+    onMouseEnter(event: MouseEvent): void {
+    }
+
+    onMouseLeave(event: MouseEvent): void {
     }
 
     before(): void {
