32b5778bb80114fd12cf9a58d54e3e2952f95ae7
Tyler Coles <shidobu@gmail.com>
2019-03-15 23:14:32 -0400

    Renamed file to match class.

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,9 +1,17 @@
 import { Engine, System, Component } from "../lib/ents";
+import { SpriteRenderer } from "./systems/spriterenderer";
 
 export default class Game {
     engine: Engine;
 
     constructor() {
         this.engine = new Engine();
+
+        
+    }
+
+    initialize() : void {
+        
+        this.engine.addSystem(new SpriteRenderer())
     }
 }
