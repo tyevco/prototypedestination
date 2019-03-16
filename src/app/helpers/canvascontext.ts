488e205a50d7776d7e0742bcfefba17b2e9ead2b
Tyler Coles <shidobu@gmail.com>
2019-03-16 12:54:27 -0400

    Added method to retrieve canvas.

--- src/app/helpers/canvascontext.ts
+++ src/app/helpers/canvascontext.ts
@@ -1,13 +1,21 @@
 
 export class CanvasContextHelper {
     private static canvasContext: CanvasRenderingContext2D = null;
+    private static canvas: HTMLCanvasElement = null;
 
     public static get2dContext() {
         if (CanvasContextHelper.canvasContext == null) {
-            let canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-            CanvasContextHelper.canvasContext = canvas.getContext("2d");
+            CanvasContextHelper.canvasContext = CanvasContextHelper.getCanvas().getContext("2d");
         }
 
         return CanvasContextHelper.canvasContext;
+    }
+
+    public static getCanvas() {
+        if (CanvasContextHelper.canvas == null) {
+            CanvasContextHelper.canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
+        }
+
+        return CanvasContextHelper.canvas;
     }
 }
