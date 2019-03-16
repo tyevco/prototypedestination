efb4219bbfed45599f935f4274fbc4cd5897ea71
Tyler Coles <shidobu@gmail.com>
2019-03-16 14:50:28 -0400

    Added some linting.

--- src/app/systems/mousemonitor.ts
+++ src/app/systems/mousemonitor.ts
@@ -1,45 +1,58 @@
 import { System } from "../../lib/ents/system";
 import { CanvasContextHelper } from "../helpers/canvascontext";
 import { IMouseHandler } from "../../lib/lang";
+import { Vector2 } from "../spatial/Vector2";
 
 
 export class MouseMonitor extends System implements IMouseHandler {
+    protected mouseDown: boolean = false;
+    protected downPosition: Vector2 = null;
+    protected currentPosition: Vector2 = Vector2.Zero;
 
     constructor() {
         super();
 
-        let canvas = CanvasContextHelper.getCanvas();
-        canvas.addEventListener("mousedown", e => this.onMouseDown(e), false);
-        canvas.addEventListener("mouseup", e => this.onMouseUp(e), false);
-        canvas.addEventListener("mousemove", e => this.onMouseMove(e), false);
-        canvas.addEventListener("mouseenter", e => this.onMouseEnter(e), false);
-        canvas.addEventListener("mouseleave", e => this.onMouseLeave(e), false);
+        let canvas: HTMLCanvasElement = CanvasContextHelper.getCanvas();
+        canvas.addEventListener("mousedown", (e: MouseEvent) => this.onMouseDown(e), false);
+        canvas.addEventListener("mouseup", (e: MouseEvent) => this.onMouseUp(e), false);
+        canvas.addEventListener("mousemove", (e: MouseEvent) => this.onMouseMove(e), false);
+        canvas.addEventListener("mouseenter", (e: MouseEvent) => this.onMouseEnter(e), false);
+        canvas.addEventListener("mouseleave", (e: MouseEvent) => this.onMouseLeave(e), false);
     }
 
     onMouseDown(event: MouseEvent): void {
         console.log(event);
 
         this.mouseDown = true;
-
+        // transform clientX/Y to worldX/Y.
+        this.downPosition = new Vector2(event.clientX, event.clientY);
     }
 
     onMouseUp(event: MouseEvent): void {
         console.log(event);
 
         this.mouseDown = false;
+
+
+        this.downPosition = null;
     }
 
     onMouseMove(event: MouseEvent): void {
-
+        this.currentPosition.set(event.clientX, event.clientY);
+        if (this.mouseDown) {
+            console.log(event.clientX + "," + event.clientY);
+        }
     }
 
     onMouseEnter(event: MouseEvent): void {
+        console.log(event);
     }
 
     onMouseLeave(event: MouseEvent): void {
+        console.log(event);
     }
 
     before(): void {
-
+        console.log("do something...");
     }
 }
