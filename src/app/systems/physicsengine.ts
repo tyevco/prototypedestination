9dc66bc8d8803fa90b53401a38432584c81c390f
Tyler Coles <shidobu@gmail.com>
2019-03-16 12:34:58 -0400

    Added method to start physics.

--- src/app/systems/physicsengine.ts
+++ src/app/systems/physicsengine.ts
@@ -1,6 +1,12 @@
 import { System, usesComponents } from "../../lib/ents/system";
 import { Physics, RigidBody } from "../components";
+import { Entity } from "../../lib/ents";
 
 
 @usesComponents(Physics.Name, RigidBody.Name)
-export class PhysicsEngine extends System { }
+export class PhysicsEngine extends System {
+
+    act(entity: Entity, physics: Physics, rigidBody: RigidBody) {
+
+    }
+}
