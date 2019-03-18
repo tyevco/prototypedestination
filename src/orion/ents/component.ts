9f6e2a907570214230ab85ac8b6a9b5003cfd2f2
Tyler Coles <shidobu@gmail.com>
2019-03-18 07:59:44 -0400

    Added an interface of IComponent to classes that extend the @component decorator.

--- src/orion/ents/component.ts
+++ src/orion/ents/component.ts
@@ -1,8 +1,13 @@
 import ComponentRegistry from "./componentregistry";
 
-export function component(constructor: Function) {
-    Object.assign(constructor.prototype, {
-        name: constructor.name,
-        register: ComponentRegistry.registerComponent(constructor.name),
-    });
+export interface IComponent {
+    name: string;
+    register: number;
+}
+
+export function component<T extends { new(...args: any[]): {} }>(constructor: T) {
+    return class extends constructor implements IComponent {
+        public readonly name: string = constructor.name;
+        public readonly register = ComponentRegistry.registerComponent(constructor.name);
+    };
 }
