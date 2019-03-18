6eaa860fe692daa01f0c45753434d44f0431472a
Tyler Coles <shidobu@gmail.com>
2019-03-18 11:33:38 -0400

    Added a contains and intersects method to bounds.

--- src/orion/spatial/bounds.ts
+++ src/orion/spatial/bounds.ts
@@ -1,7 +1,18 @@
+import { Vector2 } from ".";
 
 export class Bounds {
     public Top: number;
     public Left: number;
     public Width: number;
     public Height: number;
+
+    public contains(position: Vector2): boolean {
+        return (this.Left <= position.X && position.X <= this.Left + this.Width)
+            && (this.Top <= position.Y && position.Y <= this.Top + this.Height);
+    }
+
+    public intersects(other: Bounds) : boolean {
+        // NYI
+        return false;
+    }
 }
